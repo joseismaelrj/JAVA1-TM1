64f1471bb8f908ca88ea669bcf1768c6e9c35f1a
joseismaelrj <jose.nascimento@mercadolivre.com>
2021-12-27 16:29:01 -0300

    Colocando informativo na classe
    Coloca o informativo na classe.

--- JAVA1-TM1/src/br/com/meli/EhPrimo.java
+++ JAVA1-TM1/src/br/com/meli/EhPrimo.java
@@ -4,10 +4,12 @@ public class EhPrimo {
 
         public static boolean ehPrimo(long numero){
 
-            // verifica se o numero informado é divisiveo por 2
+            // verifica se o numero informado é Par
             if (numero % 2 == 0){
                 return false;
             }
+
+            // Verifica se o número tem outro divisor alem de 1 e ele mesmo
             for (long i=3; i<numero; i++){
                 if (numero % i == 0){
                     return false;
